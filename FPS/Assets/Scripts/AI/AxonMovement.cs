e9a3496281464788f2bd2d42cec1dfb06e867e56
Evan <daley.evan@gmail.com>
2016-12-30 11:04:15 -0800

    S1E14_Commit1
    In our last episode we moved Axon toward the player.

--- FPS/Assets/Scripts/AI/AxonMovement.cs
+++ FPS/Assets/Scripts/AI/AxonMovement.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class AxonMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	Transform playerTransform;
+	Transform axonTransform;
+
+	public float stoppingDistance = 1f;
+	public float moveSpeed = 2f;
+	private float timer1;
+
+	void Start () 
+	{
+		playerTransform = GameObject.Find ("PlayerFeet").transform;
+		axonTransform = GetComponent<Transform> ();
+		timer1 = Time.time;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		axonTransform.LookAt (playerTransform.position);
+
+		float distanceToPlayer = Vector3.Distance (axonTransform.position, playerTransform.position);
+
+		if (distanceToPlayer > stoppingDistance)
+		{
+			Move (playerTransform.position);
+		} else
+		{
+			Stop ();
+		}
+	}
+
+	void Move(Vector3 target)
+	{
+		if (Time.time < timer1 + 1.33f)
+		{
+			return;
+		}
+
+		print ("Moving");
+
+		axonTransform.position = Vector3.MoveTowards (axonTransform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+	}
+
+	void Stop()
+	{
+		print ("Stopped");
 	}
 }
