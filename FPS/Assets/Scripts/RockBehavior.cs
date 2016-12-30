a192957b42ed2858465be911c0982e6ffb9bfa3b
Evan <daley.evan@gmail.com>
2016-12-30 09:24:42 -0800

    S1E13_Commit1
    Edited the material, removed script from rock, named Axon

---
+++ FPS/Assets/Scripts/RockBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBehavior : MonoBehaviour {
+
+	Transform myTransform;
+	Transform playerTransform;
+
+	void Start () 
+	{
+		myTransform = GetComponent<Transform> ();
+		playerTransform = GameObject.Find ("FPSController").transform;
+	}
+	
+	void Update () 
+	{
+		myTransform.LookAt (playerTransform.position);
+	}
+}
