72c7c4ff894d9b1048c2bdf4f6f6ae8d6f93c411
Evan <daley.evan@gmail.com>
2016-12-31 19:57:52 -0800

    S1E25_Commit1

---
+++ FPS/Assets/Scripts/Character/ScoreKeeperUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeperUI : MonoBehaviour {
+
+	public Text scoreText;
+	public Text highScoreText;
+
+	void Update () 
+	{
+		scoreText.text = ScoreKeeper.Instance.score.ToString(); 
+		highScoreText.text = ScoreKeeper.Instance.highScore.ToString ();
+	}
+}
